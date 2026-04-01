0bdd51eb4d046e974d8026c1d7632e4d4b00989c
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2026-04-01 12:09:10 +0200

    release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ configure<ApplicationExtension> {
         applicationId = "com.xenonware.notes"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.5.20"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "XENON_COMMONS_VERSION", "\"${libs.versions.xenonCommons.get()}\"")
